ea9349686935404c871745afa5f49d712c044da3
jemmyasjd <jemmy33.jd@gmail.com>
2024-02-17 23:58:39 +0530

    Added the genisi block

---
+++ config.js
@@ -0,0 +1,11 @@
+const INITIAL_DIFFICULTY=2;
+const MINE_RATE = 1000;
+const Genesis_data={
+    timestamp:'01/01/2021',
+    prevHash:'0x0000',
+    hash:'0x0001',
+    data:'100',
+    nonce: 0,
+    difficulty: INITIAL_DIFFICULTY
+};
+module.exports={Genesis_data , MINE_RATE};
