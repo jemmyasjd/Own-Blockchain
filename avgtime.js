1b4ecc8681897809931fc05661097814c5a4f8a6
jemmyasjd <jemmy33.jd@gmail.com>
2024-02-18 00:05:22 +0530

    Added the average time functionality

---
+++ avgtime.js
@@ -0,0 +1,23 @@
+const Blockchain = require('./chain')
+const blockchain = new Blockchain()
+
+let prevTimestamp, nextTimestamp, nextBlock, timeDiff, average
+
+blockchain.addBlock({ data: 'block1' });
+const times = []
+
+for (let i = 0; i < 10000; i++) {
+    prevTimestamp = blockchain.chain[blockchain.chain.length - 1].timestamp
+    
+    blockchain.addBlock({ data: `block ${i}` })
+    nextBlock = blockchain.chain[blockchain.chain.length - 1]
+    
+    nextTimestamp = nextBlock.timestamp
+    timeDiff = nextTimestamp - prevTimestamp
+    times.push(timeDiff)
+    
+    average = times.reduce((total, num) => (total + num)) / times.length
+    
+    console.log(`Time to mine block: ${timeDiff}ms. Difficulty: ${nextBlock.difficulty}. Average time: ${average}ms`)
+}
+
